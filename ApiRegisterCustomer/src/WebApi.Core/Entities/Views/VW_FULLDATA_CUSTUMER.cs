6c42d8ab8d8f3ee694f0dc92b419efdb571ef0df
Jefferson Santos <jeff.debian2016@gmail.com>
2022-07-03 14:42:04 -0300

    Add vw_fulldata_custumer.

---
+++ ApiRegisterCustomer/src/WebApi.Core/Entities/Views/VW_FULLDATA_CUSTUMER.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace WebApi.Core.Entities.Views
+{
+  public partial class VW_FULLDATA_CUSTUMER
+  {
+    public string name { get; set; }
+    public string tax_id { get; set; }
+    public string phone_number { get; set; }
+    public DateTime? created_at { get; set; }
+    public string code { get; set; }
+    public string address { get; set; }
+    public string district { get; set; }
+    public string city { get; set; }
+    public string state { get; set; }
+    public string status { get; set; }
+  }
+}
