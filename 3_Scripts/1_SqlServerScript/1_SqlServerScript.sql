abcabb08d7e2abba05c9c6d8c3df2e05f6d07a44
Jefferson Santos <jeff.debian2016@gmail.com>
2022-07-05 23:32:17 -0300

    Documentation View.

--- 3_Scripts/1_SqlServerScript/1_SqlServerScript.sql
+++ 3_Scripts/1_SqlServerScript/1_SqlServerScript.sql
@@ -1,15 +1,12 @@
 /* 
 <summary>
 /// @author: Jefferson Santos
-/// @Data  : 03/07/2022
+/// @Data  : 05/07/2022
 /// 
 /// File with the sql scripts used to create the ApiRegisterCustomer
 /// ~~> Be careful when modifying this file!
 </summary>
 */
-
--- CREATE DATABASE
-CREATE DATABASE DB_CHALLENGE_BC;
 
 -- ~~> DROP DATABASE <~~
 DROP DATABASE DB_CHALLENGE_BC;  
@@ -19,31 +16,15 @@ DROP TABLE TB_STATUS_ADDRESS;
 DROP TABLE TB_CUSTUMER;
 DROP TABLE TB_ADDRESS;
 
--- ~~> EXTRA RESOURCESS  
-SELECT CONVERT (datetime, GETDATE())
+-----> 1º
+-- CREATE DATABASE
+CREATE DATABASE DB_CHALLENGE_BC;
 
-
-SELECT S.NAME       AS 'Schema',
-       T.NAME       AS Tabela,
-       C.NAME       AS Coluna,
-       TY.NAME      AS Tipo,
-       C.max_length AS 'Tamanho Máximo',-- Tamanho em bytes, para nvarchar normalmente se divide este valor por 2
-       C.PRECISION  AS 'Precisão',-- Para tipos numeric e decimal (tamanho)
-       C.scale      AS 'Escala'
--- Para tipos numeric e decimal (números após a virgula)
-FROM   sys.columns C
-       INNER JOIN sys.tables T
-               ON T.object_id = C.object_id
-       INNER JOIN sys.types TY
-               ON TY.user_type_id = C.user_type_id
-       LEFT JOIN sys.schemas S
-              ON T.schema_id = S.schema_id 
---  EXTRA RESOURCESS <~~ 
-
-
+-----> 2º
 -- USE DATABASE
 USE DB_CHALLENGE_BC;
 
+-----> 3º
 -- CREATE TABLE CUSTOMER
 CREATE TABLE TB_CUSTUMER(
     id uniqueidentifier primary key,
@@ -54,6 +35,14 @@ CREATE TABLE TB_CUSTUMER(
     created_at datetime DEFAULT GETDATE()
 );
 
+-----> 4º
+-- CREATE TABLE STATUS
+CREATE TABLE TB_STATUS_ADDRESS(
+   id uniqueidentifier primary key,
+   status varchar(10)
+);
+
+-----> 5º
 -- CREATE TABLE ADDRESS
 CREATE TABLE TB_ADDRESS(
    id uniqueidentifier primary key,
@@ -66,12 +55,7 @@ CREATE TABLE TB_ADDRESS(
    code varchar(200)
 );
 
--- CREATE TABLE STATUS
-CREATE TABLE TB_STATUS_ADDRESS(
-   id uniqueidentifier primary key,
-   status varchar(10)
-);
-
+-----> 6º
 -- CREATE VIEW ALL DATE
 CREATE VIEW VW_FULLDATA_CUSTUMER
 AS
@@ -83,65 +67,15 @@ inner join TB_STATUS_ADDRESS s
 ON s.id = a.id_status_address;
 
 
-
--- SELECT VW_FULLDATA_CUSTUMER
-SELECT * FROM VW_FULLDATA_CUSTUMER;
-SELECT * FROM VW_FULLDATA_CUSTUMER_IS_PENDING
-SELECT * FROM VW_FULLDATA_CUSTUMER_IS_APPROVED
-
 -- SELECTS
 SELECT * FROM TB_CUSTUMER;
 SELECT * FROM TB_STATUS_ADDRESS;
 SELECT * FROM TB_ADDRESS;
 
---> CREATE DATA FOR EXAMPLAS TABLE CUSTUMER
-INSERT INTO dbo.TB_CUSTUMER(id,name,tax_id,password,phone_number) VALUES('99c2418e-d047-4d3e-9aec-1cb8642b1960','Marcos Silva','12335476804','123456','81987881234')
-INSERT INTO dbo.TB_CUSTUMER(id,name,tax_id,password,phone_number) VALUES('c8fabecd-81a5-4c3c-bb86-f9980336ffb0','Morgana Rocha','12335476804','123456','81987881234')
+-- SELECT VW_FULLDATA_CUSTUMER
+SELECT * FROM VW_FULLDATA_CUSTUMER;
 
--- ~~> APPROVED
-INSERT INTO dbo.TB_CUSTUMER(id,name,tax_id,password,phone_number) VALUES('38e4a1b2-130e-4f6f-a587-a0ea91e8c929','Tiago Chavier','12335476804','123456','81987881234')
-
-
---> CREATE DATA FOR EXAMPLAS TABLE STATUS_ADDRESS
-INSERT INTO dbo.TB_STATUS_ADDRESS(id,status) VALUES('5d5e3f3c-9e18-429b-ade8-9042bcdaf19a','PENDING')
-INSERT INTO dbo.TB_STATUS_ADDRESS(id,status) VALUES('fc56d792-21d1-43a7-96bc-a3fa0e39b6d9','PENDING')
-
--- ~~> APPROVED
-INSERT INTO dbo.TB_STATUS_ADDRESS(id,status) VALUES('12b24eb4-4c25-4c2c-86e6-0252d5bc4853','APPROVED')
-
-
---> CREATE DATA FOR EXAMPLAS TABLE ADDRESS
-INSERT INTO dbo.TB_ADDRESS(id,id_custumer,id_status_address) 
-VALUES
-('77a037ee-5ee7-45b1-ab4f-3585b48374d5','99c2418e-d047-4d3e-9aec-1cb8642b1960','5d5e3f3c-9e18-429b-ade8-9042bcdaf19a')
-
-INSERT INTO dbo.TB_ADDRESS(id,id_custumer,id_status_address) 
-VALUES
-('d0c903e4-6aa0-48f3-aec3-003c62fa9e56','c8fabecd-81a5-4c3c-bb86-f9980336ffb0','fc56d792-21d1-43a7-96bc-a3fa0e39b6d9')
-
--- ~~> APPROVED
-INSERT INTO dbo.TB_ADDRESS(id,id_custumer,id_status_address,state,city,district,address,code) 
-VALUES
-('e6cb18a1-258a-4d28-8de1-b4029a34b3c6','38e4a1b2-130e-4f6f-a587-a0ea91e8c929','12b24eb4-4c25-4c2c-86e6-0252d5bc4853',
- 'SP','SÃO PAULO','CENTRO','Av. São João, 439','01035-000'
-)
-    
-
---> rascunhos
-SELECT * FROM TB_CUSTUMER
-SELECT * FROM TB_ADDRESS
-SELECT * FROM TB_STATUS_ADDRESS
-
-SELECT * FROM VW_FULLDATA_CUSTUMER where created_at  BETWEEN  '03-07-2022' AND '03-07-2022 23:59:59';
-
-{07/03/2022 00:00:00}
-
-select * from VW_FULLDATA_CUSTUMER where name like 'Tiago Chavier'
-
-select * from VW_FULLDATA_CUSTUMER  where datediff(day, '03-07-2022', '03-07-2022') =0
-
-
-delete from TB_CUSTUMER
-delete from TB_STATUS_ADDRESS
-delete from TB_ADDRESS
-
+-- DELETE DATA FROM TABLE
+DELETE FROM TB_ADDRESS
+DELETE FROM TB_CUSTUMER
+DELETE FROM TB_STATUS_ADDRESS
