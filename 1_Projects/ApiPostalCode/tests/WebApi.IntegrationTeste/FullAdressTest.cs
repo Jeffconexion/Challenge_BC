b509f59320ead5f35076a514907385a313a931bf
Jefferson Santos <jeff.debian2016@gmail.com>
2022-08-11 22:03:23 -0300

    Add integration test.

---
+++ 1_Projects/ApiPostalCode/tests/WebApi.IntegrationTeste/FullAdressTest.cs
@@ -0,0 +1,52 @@
+﻿using System.Text.Json;
+using System.Threading.Tasks;
+using FluentAssertions;
+using WebApi.Core.Dtos;
+using WebApi.IntegrationTeste.Config;
+using WebApi.PostalCode;
+using Xunit;
+
+namespace WebApi.IntegrationTeste
+{
+  [Collection(nameof(IntegrationApiTestsFixtureCollection))]
+  public class FullAdressTest
+  {
+    private readonly IntegrationTestsFixture<StartupApiTest> _testsFixture;
+
+    public FullAdressTest(IntegrationTestsFixture<StartupApiTest> testsFixture)
+    {
+      _testsFixture = testsFixture;
+    }
+
+    [Fact(DisplayName = "Search Postal Code With Successfully")]
+    [Trait("Postal Code", "Outsourcing Postal Code Tests")]
+    public async Task PostalCode_Find_CanBeTrue()
+    {
+      //act
+      var getPostalCodeResponse = await _testsFixture.Client.GetAsync($"api/v1/postalCode/" + "01035-000");
+
+      var jsonResponse = getPostalCodeResponse.Content.ReadAsStringAsync().Result;
+      var postalCode = JsonSerializer.Deserialize<PostalCodeDtos>(jsonResponse);
+
+      //assert
+      postalCode.Ok.Should().BeTrue().And.Be(true);
+      postalCode.Should().NotBeNull();
+
+    }
+
+    [Fact(DisplayName = "Search Postal Code Not Successfully")]
+    [Trait("Postal Code", "Outsourcing Postal Code Tests")]
+    public async Task PostalCode_Find_CanBeFalse()
+    {
+      //act
+      var getPostalCodeResponse = await _testsFixture.Client.GetAsync($"api/v1/postalCode/" + "00000-000");
+
+      var jsonResponse = getPostalCodeResponse.Content.ReadAsStringAsync().Result;
+      var postalCode = JsonSerializer.Deserialize<PostalCodeDtos>(jsonResponse);
+
+      //assert
+      postalCode.Ok.Should().BeFalse().And.Be(false);
+      postalCode.Status.Should().Be(404);
+    }
+  }
+}
